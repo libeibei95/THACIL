6738fce9c578b8dccc41102272d3e35ed37a549e
libeibei95 <573328344@qq.com>
2021-06-02 23:56:40 +0800

    fix bug

--- src/solver.py
+++ src/solver.py
@@ -126,7 +126,7 @@ class Solver(object):
                 # regenerate negative buffers
 
                 self.data.close_train_processes()
-                self.data.neg_buffers = self.data.pre_sample_negs()
+                # self.data.neg_buffers = self.data.pre_sample_negs()
                 self.data.initTrainProcess()
 
                 if stop_training_counter > 5:
